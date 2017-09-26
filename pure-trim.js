f938fb2d9e8598b11318063c9e998c79accd34f9
Prosen-Ghosh <prosenghosh25@gmail.com>
2017-09-26 18:43:18 +0600

    function stracture changed

--- pure-trim.js
+++ pure-trim.js
@@ -1,25 +1,35 @@
 var pureTrim = (function(){
     'use strict';
-	const protoStr = Object.prototype.toString;
-    function s_trim(str){
+    const protoStr = Object.prototype.toString;
+    
+    var isObject = function(value){
+        return typeof  value === 'object' && protoStr.call(value) !== '[object Array]';
+    }
+    var isArray = function(value){
+        return protoStr.call(value) === '[object Array]';
+    }
+    var isString = function(str){
+        return typeof str === 'string';
+    }
+    var s_trim = function(str){
         return str.replace(/^\s+|\s+$/g, "").replace(/\s\s+/g,' ');
     }
-    function trim(value){
+    var trim = function(value){
     	value = value || '';
 
-    	if(protoStr.call(value) === '[object Array]'){
+    	if(isArray(value)){
     		return value.map(function(v){
-                if(protoStr.call(v) === '[object Array]')return trim(v);
-				if(typeof  v === 'object' && protoStr.call(v) !== '[object Array]')return trim(v);
-    			if(typeof v === 'string')return s_trim(v);
+                if(isArray(v))return trim(v);
+				else if(isObject(v))return trim(v);
+    			else if(isString(v))return s_trim(v);
     			else return v;
     		});
     	}
-    	if(typeof value === 'object' && protoStr.call(value) !== '[object Array]'){
+    	if(isObject(value)){
             for(let key in value){
-                if(typeof  value[key] === 'object' && protoStr.call(value[key]) !== '[object Array]')value[key] = trim(value[key]);
-				if(protoStr.call(value[key]) === '[object Array]')value[key] = trim(value[key]);
-                if(typeof value[key] === 'string')value[key] = s_trim(value[key]);
+                if(isObject(value[key]))value[key] = trim(value[key]);
+				else if(isArray(value[key]))value[key] = trim(value[key]);
+                else if(isString(value[key]))value[key] = s_trim(value[key]);
             }
             return value;
     	}
